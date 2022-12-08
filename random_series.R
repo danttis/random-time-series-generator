bdc8fea8e3b6f2ecd4d7911224227bc4ab04912e
Dante <mitcrushing@gmail.com>
2022-12-08 14:07:47 -0300

    removal of explicit ties

--- random_series.R
+++ random_series.R
@@ -1,17 +1,13 @@
+cat("start of series in yyyy-mm-dd format")
+data = as.Date(scan(what = " ", nmax = 1))
 cat("what series size do you want to get?")
 size = scan(nmax=1)
-year = sample(2000:2022, size, replace=TRUE)
-month = sample(1:12, size, replace=TRUE)
-day = sample(1:31, size, replace=TRUE)
-date = NULL
-hour = NULL
-target = NULL
-for(i in 1:size){
-  date[i] = paste(year[i],month[i],day[i],sep = "-")
-  hour[i] = paste(sample(1:12, 1),sample(1:60,1),
-                  sample(1:60, 1),sep = ":")
-  target[i] = sample(1:1776, 1)
-}
-
-table <- data.frame(date = date, hour=hour,target = target)
+cat("minimum value")
+minimum = scan(nmax=1)
+cat("minimum value")
+maximum  = scan(nmax=1)
+x = seq(1,size)
+values = runif(size, min=minimum, max=maximum)
+date = data + x
+table <- data.frame(date = date, target = values)
 write.csv(table, "random_series.csv", row.names = FALSE) 
